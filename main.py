d68067c3fc03a2efca9f3f0d4052ca21708f3338
Mathiasbr05 <mathias19082005@gmail.com>
2022-11-07 22:01:00 +0100

    init

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from colorama import Fore
+class Fore:
+    BLACK     = "\033[30m"
+    RED       = "\033[31m"
+    GREEN     = "\033[32m"
+    YELLOW    = "\033[33m"
+    BLUE      = "\033[34m"
+    MAGENTA   = "\033[35m"
+    CYAN      = "\033[36m"
+    WHITE     = "\033[37m"
+    UNDERLINE = "\033[4m"
+    RESET     = "\033[0m"
 
 mode = "Menu"
 modes = ("Menu", "Length", "Speed", "Mass", "Temperature")
